3a63f0f8f65783ff535cfbcfe07cd2ec2c7232a4
g-akca <gok.akca66@gmail.com>
2026-03-18 00:59:39 +0300

    Add Header structure and styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
+import menuIcon from "/images/icon-menu.svg";
+import logoImg from "/images/logo.svg";
+import cartIcon from "/images/icon-cart.svg";
+import avatarImg from "/images/image-avatar.png";
+
 function Header() {
   return (
-    <>
-    </>
+    <header className="px-6 pb-6 pt-5 flex justify-between items-center">
+      <div className="h-5 flex gap-4 items-end">
+        <img src={menuIcon} alt="Menu icon" className="h-3.75" />
+        <img src={logoImg} alt="Logo titled sneakers" className="h-full" />
+      </div>
+
+      <div className="h-6 flex gap-6 items-center">
+        <img src={cartIcon} alt="Cart icon" className="h-5" />
+        <img src={avatarImg} alt="User avatar" className="h-full" />
+      </div>
+    </header>
   )
 }
 
