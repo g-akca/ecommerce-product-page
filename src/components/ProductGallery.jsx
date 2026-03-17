f4e4df7c55345ae79468e69ce50cbb6477ad5ffe
g-akca <gok.akca66@gmail.com>
2026-03-18 01:25:00 +0300

    Add ProductGallery structure and styles

--- src/components/ProductGallery.jsx
+++ src/components/ProductGallery.jsx
@@ -1,7 +1,19 @@
+import previousIcon from "/images/icon-previous.svg";
+import nextIcon from "/images/icon-next.svg";
+
 function ProductGallery() {
   return (
-    <>
-    </>
+    <section className="h-75 p-4 flex items-center bg-[url('/images/image-product-1.jpg')] bg-cover bg-center">
+      <div className="grow flex justify-between items-center">
+        <button className="w-10 h-10 bg-white rounded-full flex justify-center items-center cursor-pointer">
+          <img src={previousIcon} alt="Previous icon" className="w-2.5" />
+        </button>
+        
+        <button className="w-10 h-10 bg-white rounded-full flex justify-center items-center cursor-pointer">
+          <img src={nextIcon} alt="Next icon" className="w-2.5" />
+        </button>
+      </div>
+    </section>
   )
 }
 
