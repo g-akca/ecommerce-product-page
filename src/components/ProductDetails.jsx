dd5d0e5a1af5a889cb28c217cc90dc306117a4ff
g-akca <gok.akca66@gmail.com>
2026-03-18 22:46:02 +0300

    Add desktop responsive ProductDetails styles

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -5,10 +5,10 @@ import plusIcon from "/images/icon-plus.svg";
 
 function ProductDetails() {
   return (
-    <section className="px-6 flex flex-col gap-8 tablet:px-0 tablet:gap-6">
+    <section className="px-6 flex flex-col gap-8 tablet:px-0 tablet:gap-6 desktop:gap-8">
       <div>
-        <p className="mb-4 uppercase text-[12px] tracking-[1.85px] leading-auto font-bold tablet:mb-6 tablet:text-[13px] tablet:tracking-[2px]">Sneaker company</p>
-        <h2 className="mb-4 text-[28px] leading-8 font-bold text-grey-950 tablet:mb-6 tablet:text-[44px] tablet:leading-12">Fall Limited Edition Sneakers</h2>
+        <p className="mb-4 uppercase text-[12px] tracking-[1.85px] leading-auto font-bold tablet:mb-6 tablet:text-[13px] tablet:tracking-[2px] desktop:leading-4">Sneaker company</p>
+        <h2 className="mb-4 text-[28px] leading-8 font-bold text-grey-950 tablet:mb-6 tablet:text-[44px] tablet:leading-12 desktop:mb-8">Fall Limited Edition Sneakers</h2>
 
         <p className="text-[15px] tablet:text-base">These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
       </div>
@@ -22,8 +22,8 @@ function ProductDetails() {
         <p className="line-through font-bold">$250.00</p>
       </div>
 
-      <div className="flex flex-col gap-4 font-bold text-grey-950 tablet:flex-row tablet:gap-2">
-        <div className="bg-grey-50 rounded-[10px] p-4 h-14 flex justify-between items-center tablet:w-75 tablet:px-6">
+      <div className="flex flex-col gap-4 font-bold text-grey-950 tablet:flex-row tablet:gap-2 desktop:gap-4">
+        <div className="bg-grey-50 rounded-[10px] p-4 h-14 flex justify-between items-center tablet:w-75 tablet:px-6 desktop:w-39.25 desktop:px-2">
           <button>
             <img src={minusIcon} alt="Minus icon" />
           </button>
